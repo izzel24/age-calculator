4ed6317ff36d49e22c0c69be7b54ed1dd89c492d
izzel24 <izzzzzzell2401@gmail.com>
2023-11-14 01:17:32 +0700

    Update dikit gak ngaruh

--- script.js
+++ script.js
@@ -29,58 +29,61 @@ function checker() {
     let months = parseInt(form1.month.value, 10);
     let years = parseInt(form1.year.value, 10);
     let leapyear;
+    
     if (years % 4 === 0 && (years % 100 !== 0 || years % 400 === 0)) {
         leapyear = true;
     } else {
         leapyear = false;
     }
-    
-    console.log('Is it a leap year?', leapyear);
-    console.log(new Date().getFullYear());
+
     let dayonmonth;
+
     if (months === 2) {
         if (leapyear === true) {
             dayonmonth = 29;
-            console.log('It is a leap year!');
         } else {
             dayonmonth = 28;
-            console.log('It is not a leap year.');
         }
-    }if(months!== 2 && months % 2 === 0 && months !== 8){
+    } else if (months !== 2 && months % 2 === 0 && months !== 8) {
         dayonmonth = 30;
-    }else if(months !== 2 && months % 2 !=0 || months === 8){
+    } else if (months !== 2 && (months % 2 !== 0 || months === 8)) {
         dayonmonth = 31;
     }
-    console.log("months:",months);
-    console.log(dayonmonth);
+
     labels.forEach((label, index) => {
-        let birthdate = new Date(`${months}/${days}/${years}`);
-        let currentdate = new Date();
-        if (days === "" && months === "" && years === "") {
+        if (isNaN(days) && isNaN(months) && isNaN(years)) {
             label.classList.add("labelerr");
-            errorAlerts[index].innerText = "This field is requiered";
-            successElements[index].classList.toggle("success");
-        }else if(birthdate > currentdate){
+            errorAlerts[index].innerText = "This field is required";
+            successElements[index].classList.remove("success");
+        }else if(isNaN(days) && !isNaN(months) && !isNaN(years)){
             label.classList.add("labelerr");
-            errorAlerts[index].innerText = "Must be valid date";
-            successElements[index].classList.toggle("success");
-        }else if(days > 31){
+            errorAlerts[0].innerText = "This field is required";
+            successElements[index].classList.remove("success");
+        }else if(!isNaN(days) && isNaN(months) && !isNaN(years)){
             label.classList.add("labelerr");
-            errorAlerts[0].innerText = "Must be valid date";
-            successElements[index].classList.toggle("success");
-        }else if(days > dayonmonth){
+            errorAlerts[1].innerText = "This field is required";
+            successElements[index].classList.remove("success");
+        }else if(!isNaN(days) && !isNaN(months) && isNaN(years)){
             label.classList.add("labelerr");
-            errorAlerts[0].innerText = "Must be valid date";
-            successElements[index].classList.toggle("success");
-        }else if(months > 12){
+            errorAlerts[2].innerText = "This field is required";
+            successElements[index].classList.remove("success");
+        }else if (days > 31 || days <= 0) {
             label.classList.add("labelerr");
-            errorAlerts[1].innerText = "Must be valid date";
-            successElements[index].classList.toggle("success");
-        }else if(years > new Date().getFullYear()){
+            errorAlerts[0].innerText = "Must be a valid date";
+            successElements[index].classList.remove("success");
+        } else if (days > dayonmonth) {
             label.classList.add("labelerr");
-            errorAlerts[2].innerText = "Must be valid date";
-            successElements[index].classList.toggle("success");
-        }else if(days !="" && months !="" && years !="") {
+            errorAlerts[0].innerText = "Must be a valid date";
+            successElements[index].classList.remove("success");
+        } else if (months > 12) {
+            label.classList.add("labelerr");
+            errorAlerts[1].innerText = "Must be a valid date";
+            successElements[index].classList.remove("success");
+        } else if (years > new Date().getFullYear()) {
+            label.classList.add("labelerr");
+            errorAlerts[2].innerText = "Must be a valid date";
+            successElements[index].classList.remove("success");
+        } else if (!isNaN(days) && !isNaN(months)  && !isNaN(years)) {
             label.classList.remove("labelerr");
             errorAlerts[index].innerText = ""; 
             successElements[index].classList.add("success");
